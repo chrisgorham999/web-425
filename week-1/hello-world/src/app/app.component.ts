d16fe603a1fffe81655351e44784ebe5adc6047d
Chris Gorham <chris.gorham@outlook.com>
2023-05-28 18:00:26 -0400

    week 1 work

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myWorld = 'You are now in Gorhams World';
+  title = 'hello-world';
 }
