219d76796b1899ec954184284696473c9c80fcc0
Chris Gorham <chris.gorham@outlook.com>
2023-05-28 18:22:31 -0400

    attribution updates

--- week-1/hello-world/src/app/app.component.spec.ts
+++ week-1/hello-world/src/app/app.component.spec.ts
@@ -1,3 +1,15 @@
+/*
+======================================
+; Title: app.component.spec.ts
+; Author: Chris Gorham
+; Date: 28 May 2023
+; Description: This code supports functionality for the hello-world project
+; Sources Used:
+; Assignment 1.3 Instructions
+; Angular CLI Hello World YouTube Video (https://youtu.be/XRm0SnUJROU)
+;=====================================
+*/
+
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
