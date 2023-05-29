2b380fd7120310f211ba259b5717bd8378cc2b46
Chris Gorham <chris.gorham@outlook.com>
2023-05-28 21:21:55 -0400

    attribution updates (final)

--- week-1/profile-app/src/app/app-routing.module.ts
+++ week-1/profile-app/src/app/app-routing.module.ts
@@ -1,3 +1,15 @@
+/*
+======================================
+; Title: app-routing.module.ts
+; Author: Chris Gorham
+; Date: 28 May 2023
+; Description: This code supports functionality for the profile picture app
+; Sources Used:
+; Assignment 1.5 Instructions
+; Angular CLI Hello World YouTube Video (https://youtu.be/XRm0SnUJROU)
+;=====================================
+*/
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
