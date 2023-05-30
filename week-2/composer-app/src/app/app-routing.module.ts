ca02c4a05bb34bd2ff599ff6f3e48cbccb4b6f84
Chris Gorham <chris.gorham@outlook.com>
2023-05-30 12:21:44 -0400

    week 2 updates

--- week-2/composer-app/src/app/app-routing.module.ts
+++ week-2/composer-app/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'composer-list', component: ComposerListComponent }, {path: 'contact', component: ContactComponent }, {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- week-2/composer-app/src/app/app.module.ts
+++ week-2/composer-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComposerListComponent } from './composer-list/composer-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposerListComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
