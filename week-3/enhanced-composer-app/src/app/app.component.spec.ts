c1cfbd9d351ecab1700486043ae397d3f096e9c8
Chris Gorham <chris.gorham@outlook.com>
2023-06-05 19:19:12 -0400

    attribution & comment updates

--- week-3/enhanced-composer-app/src/app/app.component.spec.ts
+++ week-3/enhanced-composer-app/src/app/app.component.spec.ts
@@ -5,7 +5,7 @@
 ; Date: 05 June 2023
 ; Description: This code supports functionality for the enhanced composer app
 ; Sources Used:
-; Exercise 2.4 Instructions
+; Exercise 3.2 Instructions
 ;=====================================
 */
 
